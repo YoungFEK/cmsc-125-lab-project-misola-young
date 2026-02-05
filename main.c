db22a5ab09fb07afdaa33b5eb1bb8593012bf5a8
YoungFEK <197771340+YoungFEK@users.noreply.github.com>
2026-02-05 10:46:25 +0800

    feature: adding struct

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
 
+typedef struct {
+    char *command;        // Command name
+    char *args[256];      // Arguments (NULL-terminated)
+    char *input_file;     // For < redirection (NULL if none)
+    char *output_file;    // For > or >> redirection (NULL if none)
+    bool append;          // true for >>, false for >
+    bool background;      // true if & present
+} Command;
+
+
 int main() {
+    //create a struct object/instance out of user input
+    struct Command cmd;
+
     
 }
