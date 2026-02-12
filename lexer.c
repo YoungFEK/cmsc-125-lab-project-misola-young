786d2407944a6a8e94863767c5906bb4d37c5563
YoungFEK <197771340+YoungFEK@users.noreply.github.com>
2026-02-12 21:58:49 +0800

    feat: code(commented) adding space to user input
    commented code for handling various commands with various formats

---
+++ lexer.c
@@ -0,0 +1,46 @@
+ // make me a for loop that loops through a character array and copies its contents in a second array, whenever it encounters these characters: ">>", ">", "<", "&" it checks if they have spaces to their left and right and if not it adds them in the second character array
+    // while (user_input[user_input_index] != '\0') {
+
+    //     // Handle >>
+    //     if (user_input[user_input_index] == '>' && user_input[user_input_index + 1] == '>') {
+
+    //         // if(user_input_index > 0 && user_input[user_input_index + 2] != '\0') {
+
+    //         // }
+
+    //         if (user_input_index > 0 && user_input[user_input_index-1] != ' ') {
+    //              processed_input[processed_input_index] = ' '; 
+    //              processed_input_index++;   
+
+    //         }
+
+    //         processed_input[processed_input_index] = '>';
+    //         processed_input_index++;
+    //         processed_input[processed_input_index] = '>';
+    //         processed_input_index++;
+
+    //         user_input_index += 2;
+
+    //         if (user_input_index > 0 && user_input[user_input_index] != ' ') {
+    //             processed_input[processed_input_index] = ' '; 
+    //             processed_input_index++;   
+
+    //         }
+
+
+
+    //     }
+    //     // Handle single-character operators: > < &
+    //     else if (user_input[user_input_index] == '>' || user_input[user_input_index] == '<' || user_input[user_input_index] == '&') {
+
+            
+    //     }
+    //     else {
+    //         // Normal character
+    //         processed_input[processed_input_index++] = user_input[user_input_index];
+    //         user_input_index++;
+
+    //     }
+    // }
+
+    // dest[j] = '\0';
