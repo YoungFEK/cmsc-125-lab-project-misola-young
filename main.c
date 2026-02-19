92e2898e978a6be77f62f731807631bf0c8205d4
YoungFEK <197771340+YoungFEK@users.noreply.github.com>
2026-02-19 08:39:14 +0800

    fix: freed allocated memory

--- main.c
+++ main.c
@@ -1,19 +1,63 @@
+//main.c
 #include <stdio.h>
-#include <unistd.h>
-
-typedef struct {
-    char *command;        // Command name
-    char *args[256];      // Arguments (NULL-terminated)
-    char *input_file;     // For < redirection (NULL if none)
-    char *output_file;    // For > or >> redirection (NULL if none)
-    bool append;          // true for >>, false for >
-    bool background;      // true if & present
-} Command;
-
+#include "parser.h"
+#include "command.h"
+#include <stdlib.h> 
 
 int main() {
-    //create a struct object/instance out of user input
-    struct Command cmd;
 
+    Command statement;
+    statement = parse();
+
+    int args_index0 = 0;
+    if(statement.command != NULL){
+        printf("command: %s\n", statement.command);
+        printf("args:\n");
+        while(statement.args[args_index0] != NULL){
+            printf("%s\n", statement.args[args_index0]);
+            args_index0++;
+        }
+        if(statement.input_file != NULL){
+            printf("append: input_file true\n");
+        }else{
+            printf("append: input_file false\n");
+        }
+        if(statement.output_file != NULL){
+            printf("append: output_file true\n");
+        }else{
+            printf("append: output_file false\n");
+        }
+        if(statement.append == true){
+            printf("append: true\n");
+        }else{
+            printf("append: false\n");
+        }
+        if(statement.background == true){
+            printf("background: true\n");
+        }else{
+            printf("background: false\n");
+        }
+         
+    }
     
+
+    if(statement.command != NULL){
+        free(statement.command);
+        int args_index = 0;
+        while(statement.args[args_index] != NULL){
+            free(statement.args[args_index]);
+            args_index++;
+        }
+
+        if(statement.input_file != NULL){
+            free(statement.input_file);
+        }
+
+        if(statement.output_file != NULL){
+            free(statement.output_file );
+        }
+    }
+    
+    
+    return 0;
 }
