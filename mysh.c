d92cfb36c00a74990a2260505b4a619d51da37c8
YoungFEK <197771340+YoungFEK@users.noreply.github.com>
2026-02-26 20:18:31 +0800

    test: removed print statements for debugging parser
    test: removed print statements for debugging parser

--- mysh.c
+++ mysh.c
@@ -30,46 +30,9 @@ int main(int argc, char *argv[]) {
             printf("mysh> ");
 
             // While the current character is NOT null
-
-            
-            
             fgets(user_input, sizeof(user_input), stdin);
-
             statement = parse(user_input);
-            
-            // execute_command(statement);
-            int args_index0 = 0;
-            if(statement.command != NULL){
-                printf("command: %s\n", statement.command);
-                printf("args:\n");
-                while(statement.args[args_index0] != NULL){
-                    printf("%s\n", statement.args[args_index0]);
-                    args_index0++;
-                }
-                if(statement.input_file != NULL){
-                    printf("append: input_file true\n");
-                }else{
-                    printf("append: input_file false\n");
-                }
-                if(statement.output_file != NULL){
-                    printf("append: output_file true\n");
-                }else{
-                    printf("append: output_file false\n");
-                }
-                if(statement.append == true){
-                    printf("append: true\n");
-                }else{
-                    printf("append: false\n");
-                }
-                if(statement.background == true){
-                    printf("background: true\n");
-                }else{
-                    printf("background: false\n");
-                }
-                
-            }
-    
-
+            execute_command(statement);
 
         }
     }else{
@@ -82,43 +45,8 @@ int main(int argc, char *argv[]) {
         char line[256];
         while (fgets(user_input, sizeof(line), file)) {
             printf("mysh> %s", user_input);
-
             statement = parse(user_input);
-            
-            // execute_command(statement);
-            int args_index0 = 0;
-            if(statement.command != NULL){
-                printf("command: %s\n", statement.command);
-                printf("args:\n");
-                while(statement.args[args_index0] != NULL){
-                    printf("%s\n", statement.args[args_index0]);
-                    args_index0++;
-                }
-                if(statement.input_file != NULL){
-                    printf("append: input_file true\n");
-                }else{
-                    printf("append: input_file false\n");
-                }
-                if(statement.output_file != NULL){
-                    printf("append: output_file true\n");
-                }else{
-                    printf("append: output_file false\n");
-                }
-                if(statement.append == true){
-                    printf("append: true\n");
-                }else{
-                    printf("append: false\n");
-                }
-                if(statement.background == true){
-                    printf("background: true\n");
-                }else{
-                    printf("background: false\n");
-                }
-                
-            }
-
-            printf("\n\n");
-            
+            execute_command(statement);
 
         }
 
