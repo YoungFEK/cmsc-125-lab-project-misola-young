8da7f202ee4e974d59c65876665eda870a4f13e6
Xenon5443 <mtmisola@up.edu.ph>
2026-02-26 20:54:52 +0800

    fix: Removed printf for debugging

--- mysh.c
+++ mysh.c
@@ -20,9 +20,6 @@ int main(int argc, char *argv[]) {
     Command statement;
     char user_input[256];
     int user_input_index =0;
-    
-
-    printf("output:\n");
 
     if (argc < 2) { 
         while(1){
